ea5d8d34b06983df9a816ce7467319346cf206b0
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2016-08-09 15:00:12 -0500

    added category and publication model

--- avisame_pe/settings.py
+++ avisame_pe/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'avisame_pe.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -85,7 +82,6 @@ DATABASES = {
         'HOST': os.environ['DJANGO_HOST'],
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
@@ -118,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Publication(models.Model):
+    name = models.CharField(max_length=256)
+    url = models.URLField(null=True, blank=True)
+    user = models.ForeignKey(User)
+    category = models.ForeignKey(Category)
+    latitude = models.DecimalField(decimal_places=10, max_digits=16, null=True, blank=True)
+    longitude = models.DecimalField(decimal_places=10, max_digits=16, null=True, blank=True)
+    altitude = models.DecimalField(decimal_places=10, max_digits=16, null=True, blank=True)
+    publication_creation = models.DateTimeField()
+    publication_begin = models.DateTimeField()
+    publication_end = models.DateTimeField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.name)
